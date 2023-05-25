d8f3c9ad0c3a55e4f64cec953d11974ba05d214a
Alexandru Loghin <alexandru.loghin@codemelt.com>
2023-05-25 11:10:46 +0000

    [LOO-11] Create homepage cards display with filters (mobile)

--- src/components/micro/NftCard.tsx
+++ src/components/micro/NftCard.tsx
@@ -10,37 +10,43 @@ interface Props {
 
 export const NftCard: React.FC<Props> = ({ cardTitle, itemsCount, label, cost, icon }) => {
   return (
-    <div className="h-[399px] w-64">
-      <div className="flex h-12 flex-row items-center justify-between rounded-t-xl border-2 border-solid border-[#2C3034] bg-[#1E2124]">
-        <span className="p-4 text-white">{cardTitle}</span>
-        <span className="p-4 text-[#888888]">{`${itemsCount} items`}</span>
+    <div className="xs:h-[280px] xs:w-[155px] lg:h-[399px] lg:w-64">
+      <div className="flex flex-row items-center justify-between rounded-t-xl border-2 border-solid border-[#2C3034] bg-[#1E2124] xs:h-8 lg:h-12">
+        <span className="text-white xs:p-2 xs:text-xs lg:p-4 lg:text-base">{cardTitle}</span>
+        <span className="text-[#888888] xs:p-2 xs:text-xs lg:p-4 lg:text-base">{`${itemsCount} items`}</span>
       </div>
       <div
-        className="rounded-b-xl border-2 border-solid border-[#2C3034] p-4"
+        className="rounded-b-xl border-2 border-solid border-[#2C3034] xs:p-2 lg:p-4"
         style={{
           background: "linear-gradient(360deg, #272727 0%, rgba(21, 23, 25, 0) 100%)",
         }}
       >
-        <img className="mx-auto my-0 h-[224px] w-[224px] rounded-lg" src={icon} alt="NFT icon" />
+        <img
+          className="mx-auto my-0 rounded-lg xs:h-[158px] xs:w-[158px] lg:h-[224px] lg:w-[224px]"
+          src={icon}
+          alt="NFT icon"
+        />
         <div className="flex flex-row items-center justify-between py-4">
           {label.toLowerCase() === "official" ? (
-            <div className="flex h-[23px] w-[57px] items-center justify-center rounded-[4px] border border-solid border-[#F03033] bg-[#F03033] bg-opacity-20 text-xs text-[#F03033]">
+            <div className="flex items-center justify-center rounded-[4px] border border-solid border-[#F03033] bg-[#F03033] bg-opacity-20 text-xs text-xs text-[#F03033] xs:h-4 xs:w-[43px] lg:h-[23px] lg:w-[57px]">
               {label}
             </div>
           ) : (
-            <div className="flex h-[23px] w-[57px] items-center justify-center rounded-[4px] border border-solid border-[#00FFF0] bg-[#00FFF0]  bg-opacity-20 text-xs text-[#00FFF0]">
+            <div className="flex items-center justify-center rounded-[4px] border border-solid border-[#00FFF0] bg-[#00FFF0] bg-opacity-20 text-xs text-xs text-[#00FFF0] xs:h-4 xs:w-[43px]  lg:h-[23px] lg:w-[57px]">
               {label}
             </div>
           )}
 
           <div className="flex flex-row items-center justify-end gap-1 text-xs">
             <img className="h-4 w-4" src={dolarFrame} alt="Dolar svg" />
-            <span className="text-[#F03033]">{`${cost} Coins`}</span>
+            <span className="text-[#F03033] xs:text-xs lg:text-base">{`${cost} Coins`}</span>
           </div>
         </div>
-        <button className="h-[40px] w-[224px] rounded-lg bg-gradient-to-t from-[#F03033] to-[#E5383B] text-white">
-          Open
-        </button>
+        <div className="flex flex-row items-center justify-center">
+          <button className="rounded-lg bg-gradient-to-t from-[#F03033] to-[#E5383B] text-white xs:h-[29px] xs:w-[158px] xs:text-xs lg:h-[40px] lg:w-[224px] lg:text-base">
+            Open
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/micro/SearchBar.tsx
+++ src/components/micro/SearchBar.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const SearchBar: React.FC<Props> = ({ onSearchChange }) => {
   return (
     <input
-      className="h-12 w-[302px] rounded-xl border-[1px,solid,#2C3034] bg-[#1E2023] px-6 py-[14px] text-[#888888] "
+      className="rounded-xl border  border-solid border-[#2C3034] bg-[#1E2023] px-6 py-[14px] text-[#888888] xs:h-10 xs:w-[42vw] xs:justify-self-start 2xl:h-12 2xl:w-[302px] 2xl:justify-self-center "
       type="text"
       placeholder="Search..."
       onChange={onSearchChange}
