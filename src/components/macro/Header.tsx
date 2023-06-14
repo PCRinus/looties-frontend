462c2043377231eb957eb61d16fe3f89fceeaacd
Alexandru Loghin <alexandru.loghin@codemelt.com>
2023-06-14 11:42:32 +0000

    [LOO-65] Added routing and UI changes

--- src/components/macro/Header.tsx
+++ src/components/macro/Header.tsx
@@ -5,6 +5,7 @@ import HeaderInfoBar from "../micro/HeaderInfoBar";
 import { ConnectWalletButton } from "../micro/ConnectWalletButton";
 import UserInfo from "../micro/UserInfo";
 import { useWallet } from "@solana/wallet-adapter-react";
+import { Link } from "react-router-dom";
 const Header: React.FC = () => {
   const { connected } = useWallet();
 
@@ -14,7 +15,9 @@ const Header: React.FC = () => {
       style={{ background: "linear-gradient(0deg, #1f2225 0%, rgba(31, 34, 37, 0) 100%)" }}
     >
       <div className="flex-shrink-0 xs:w-1/2 2xl:w-[378px]">
-        <HeaderLogo></HeaderLogo>
+        <Link to="/">
+          <HeaderLogo></HeaderLogo>
+        </Link>
       </div>
 
       <div className="flex-shrink-1 h-full flex-grow flex-nowrap xs:hidden 2xl:block">
--- src/components/micro/ClassicButton.tsx
+++ src/components/micro/ClassicButton.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Star from "../../assets/Star.svg";
+import StarRed from "../../assets/StarRed.svg";
+import { useLocation } from "react-router-dom";
 
 const ClassicButton: React.FC = () => {
+  const location = useLocation();
+  const isClassicPage = location.pathname === "/classic";
   return (
     <div>
-      <button className="flex items-center justify-center rounded-xl border border-custom_gray_1 bg-custom_gray_1 xs:h-10 xs:w-[116px] 2xl:h-12 2xl:w-[136px]">
-        <img src={Star} alt="star-svg-icon" />
-        <span className="ml-2 font-sans text-base font-semibold text-custom_gray_2">Classic</span>
+      <button
+        className={`flex items-center justify-center rounded-xl bg-opacity-20 xs:h-10 xs:w-[116px] 2xl:h-12 2xl:w-[136px] ${
+          isClassicPage ? "border border-red-500 bg-red-500" : "bg-custom_gray_1"
+        }`}
+      >
+        <img className="h-5 w-5" src={isClassicPage ? StarRed : Star} alt="star-svg-icon" />
+        <span
+          className={`ml-2 font-sans text-base font-semibold ${
+            isClassicPage ? "text-custom_red_1" : "text-custom_gray_2"
+          }`}
+        >
+          Classic
+        </span>
       </button>
     </div>
   );
