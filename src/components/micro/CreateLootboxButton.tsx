3087911617978c2854dbfa17559b5827fe12c577
Alexandru Loghin <alexandru.loghin@codemelt.com>
2023-07-06 14:42:27 +0000

    [LOO-131] Fixed create a lootbox button style for mobile view

--- src/components/micro/CreateLootboxButton.tsx
+++ src/components/micro/CreateLootboxButton.tsx
@@ -4,18 +4,12 @@ import CreateLootboxIcon from "../../assets/CreateLootboxIcon.svg";
 export const CreateLootboxButton = () => {
   return (
     <Link to="/create-lootbox">
-             <div
-                    className="flex cursor-pointer leading-4 px-[10px] h-[44.57px] bg-gradient-to-t from-red-700 to-red-500 font-semibold text-white justify-center items-center font-sans rounded-xl xs:h-10 xs:w-10 md:max-2xl:h-12 md:max-2xl:w-[176px] 2xl:h-12 2xl:w-[186px]"
-                >
-                    <img src={CreateLootboxIcon} alt="shield-icon-svg" />
-                    <button className="px-[4px] h-[44.57px] rounded-lg font-semibold text-white flex justify-center items-center font-sans">
-                        Create a lootbox
-                    </button>
-                </div>
-   </Link>
+      <div className="flex h-[44.57px] cursor-pointer items-center justify-center rounded-xl bg-gradient-to-t from-red-700 to-red-500 px-[10px] font-sans font-semibold leading-4 text-white xs:h-10 xs:w-full md:max-2xl:h-12 md:max-2xl:w-[176px] 2xl:h-12 2xl:w-[186px]">
+        <img src={CreateLootboxIcon} alt="shield-icon-svg" />
+        <button className="flex h-[44.57px] items-center justify-center rounded-lg px-[4px] font-sans font-semibold text-white">
+          Create a lootbox
+        </button>
+      </div>
+    </Link>
   );
 };
-
-
-
-
