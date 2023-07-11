1bc91a4e8e34a072eada012de7cf4314d785b3b3
florian chitu <chituflorian90@yahoo.com>
2023-07-11 19:02:23 +0300

    feature: transaction integrated with backend

--- src/components/micro/TableDepositsTransaction.tsx
+++ src/components/micro/TableDepositsTransaction.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 
 // Define the Game type
-interface Game {
+interface Transaction {
   id: number;
-  game: string;
-  betAmount: number;
-  winning: number;
-  date: string;
+  type: string;
+  hash: string;
+  coinsAmount: string;
+  nftName: string;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  userId: string;
 }
 
 // Define the props type for TableGameHistory component
-interface TableGameHistoryProps {
-  games: Game[];
+interface TableProps {
+  transactions: Transaction[];
 }
 
-const TableDepositsTransaction: React.FC<TableGameHistoryProps> = ({ games }) => {
+const TableDepositsTransaction: React.FC<TableProps> = ({ transactions }) => {
   return (
-    <div className="w-full p-8">
-      <div className="overflow-hidden rounded-xl border border-custom_gray_1">
+    <div className=" max-h-[576px] w-full p-8">
+      <div className="  max-h-[600px] overflow-scroll  rounded-xl border border-custom_gray_1">
         <table className="w-full flex-nowrap divide-y divide-custom_gray_1  font-sans text-base font-semibold text-custom_gray_2">
           <thead className=" bg-[#1E2124]">
             <tr className="h-12">
@@ -29,22 +33,23 @@ const TableDepositsTransaction: React.FC<TableGameHistoryProps> = ({ games }) =>
             </tr>
           </thead>
           <tbody className=" divide-y divide-custom_gray_1">
-            {games.slice(0, 10).map((game, index) => (
+            {transactions.map((transaction, index) => (
               <tr
                 className={`${index % 2 === 0 ? "h-12 bg-[#1A1D20]" : "h-12 bg-[#1E2124]"} ${
-                  games.length - 1 === index ? "rounded-b-lg" : ""
+                  transactions.length - 1 === index ? "rounded-b-lg" : ""
                 }`}
-                key={game.id}
+                key={transaction.id}
               >
-                <td className="whitespace-nowrap   pl-4 text-left">{game.id}</td>
-                <td className="whitespace-nowrap  text-left">{game.game}</td>
-                <td className="whitespace-nowrap  text-left">{game.betAmount}</td>
-                <td className="whitespace-nowrap  text-left">{game.winning}</td>
-                <td className="whitespace-nowrap  text-left">{game.date}</td>
+                <td className="whitespace-nowrap   pl-4 text-left">{transaction.id}</td>
+                <td className="whitespace-nowrap  text-left">{transaction.coinsAmount}</td>
+                <td className="whitespace-nowrap  text-left">SOL</td>
+                <td className="whitespace-nowrap  text-left">{transaction.coinsAmount}</td>
+                <td className="whitespace-nowrap  text-left">{transaction.updatedAt}</td>
               </tr>
             ))}
           </tbody>
         </table>
+        <div className="h-12 w-auto"></div>
       </div>
     </div>
   );
--- src/components/micro/WithdrawsTransactionsMobile.tsx
+++ src/components/micro/WithdrawsTransactionsMobile.tsx
@@ -1,7 +1,93 @@
 import React from "react";
+import Scrollbars from "react-custom-scrollbars-2";
+interface Transaction {
+  id: number;
+  type: string;
+  hash: string;
+  coinsAmount: string;
+  nftName: string;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  userId: string;
+}
 
-const WithdrawsTransactionsMobile = () => {
-  return <div></div>;
+// Define the props type for GameHistoryMobileCard component
+interface ITransactionProps {
+  transactions: Transaction[];
+}
+const WithdrawsTransactionsMobile: React.FC<ITransactionProps> = ({ transactions }) => {
+  const getStatusLabel = (status: string) => {
+    switch (status) {
+      case "PENDING":
+        return "Pending";
+      case "APPROVED":
+        return "Approved";
+      case "DECLINED":
+        return "Declined";
+      default:
+        return "UNDEFINED";
+    }
+  };
+  const getStatusColor = (status: string) => {
+    switch (status) {
+      case "PENDING":
+        return "text-[#FFC700]";
+      case "APPROVED":
+        return "text-[#42FF00]";
+      case "DECLINED":
+        return "text-[#F03033]";
+      default:
+        return "";
+    }
+  };
+  return (
+    <div className=" flex flex-col p-6">
+      <Scrollbars
+        autoHeight
+        autoHeightMin={0}
+        autoHeightMax={880} // This height can be adjusted based on the desired height of four games
+      >
+        {transactions.map((transaction, index) => (
+          <div
+            className={`flex h-[220px] w-full flex-shrink-0  items-center justify-normal rounded-lg border-[1px] border-custom_gray_1 bg-[#1E2124]  pl-6 ${
+              index < transactions.length - 1 ? "mb-4" : ""
+            }`}
+          >
+            <div className="flex flex-col gap-4" key={transaction.id}>
+              <div className="font-sans text-base font-semibold">
+                <span className="mr-3 text-[#888888]">ID</span>
+                <span className="text-[#DFDFDF]">{transaction.id}</span>
+              </div>
+              <div className="font-sans text-base font-semibold">
+                <span className="mr-3 text-[#888888]">Coins/NFT's</span>
+                {/* amount if coins, nftname if nft -> type */}
+                <span className="text-[#DFDFDF]">{transaction.coinsAmount}</span>
+              </div>
+              <div className="font-sans text-base font-semibold">
+                <span className="mr-3 text-[#888888]">Withdraw method</span>
+
+                <span className="text-[#DFDFDF]">SOL</span>
+              </div>
+              <div className="font-sans text-base font-semibold">
+                <span className="mr-3 text-[#888888]">Status</span>
+
+                <span className={`whitespace-nowrap text-left ${getStatusColor(transaction.status)}`}>
+                  {getStatusLabel(transaction.status)}
+                </span>
+              </div>
+              <div className="font-sans text-base font-semibold">
+                <span className="mr-3 text-[#888888]">Date</span>
+
+                <span className="text-[#DFDFDF]">{transaction.updatedAt}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </Scrollbars>
+      <div className="h-[40px] w-full"></div>
+    </div>
+  );
 };
 
 export default WithdrawsTransactionsMobile;
