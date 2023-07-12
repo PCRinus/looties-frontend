c1e1f337083156b2e10782b31333d048f494899a
Mircea Casapu <mircea.casapu@gmail.com>
2023-07-12 20:15:53 +0300

    feat: try it button now tries the lootbox

--- src/components/micro/TryItButton.tsx
+++ src/components/micro/TryItButton.tsx
@@ -1,8 +1,32 @@
-import React from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 const TryItButton = () => {
+  const { lootboxId } = useParams();
+  const auth = useSelector((state: any) => state.auth);
+
+  const handleTryLootbox = async (lootboxId?: string) => {
+    if (!lootboxId) {
+      toast.error("Could not fetch the lootbox ID, try again later");
+    }
+
+    try {
+      await axios.get(`${process.env.REACT_APP_API_URL}/lootbox/${lootboxId}/try-lootbox`, {
+        headers: { Authorization: `Bearer ${auth.jwt}` },
+      });
+    } catch (error) {
+      console.log("Self exclude error: ", error);
+      toast.error("Failed to try the lootbox, try again later!");
+    }
+  };
+
   return (
-    <button className="md-max:2xl:w-[98px] mr-4 flex items-center justify-center rounded-xl border border-custom_gray_1 bg-custom_gray_1 xs:mr-3 xs:h-[32px]  xs:w-[62px] xs:rounded-lg md:max-2xl:h-12 2xl:h-12 2xl:w-[98px]">
+    <button
+      className="md-max:2xl:w-[98px] mr-4 flex items-center justify-center rounded-xl border border-custom_gray_1 bg-custom_gray_1 xs:mr-3 xs:h-[32px]  xs:w-[62px] xs:rounded-lg md:max-2xl:h-12 2xl:h-12 2xl:w-[98px]"
+      onClick={() => handleTryLootbox(lootboxId)}
+    >
       <span className=" font-sans font-bold text-white xs:text-[12px] md:max-2xl:text-[20px] 2xl:text-[20px]">
         Try it
       </span>
