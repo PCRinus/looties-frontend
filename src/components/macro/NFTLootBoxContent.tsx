42197ad7db8c6cdf77ab8af9135cafd96699df1e
Alexandru Loghin <loghinalex19@gmail.com>
2023-07-12 19:31:56 +0300

    chore: comment out edit/delete buttons

--- src/components/macro/NFTLootBoxContent.tsx
+++ src/components/macro/NFTLootBoxContent.tsx
@@ -44,14 +44,14 @@ const NFTLootBoxContent = () => {
     <div className="flex flex-col justify-center">
       <span className="ml-1 flex flex-row justify-between font-sans font-bold text-custom_white_1 xs:mt-12 xs:text-[20px] 2xl:mt-[52px] 2xl:text-2xl">
         <span>NFT’S lootbox contents</span>
-        {user.id && user.profile ? (
+        {/* {user.id && user.profile ? (
           <button className="flex h-[38px] items-center justify-center gap-2 rounded-lg bg-gradient-to-t from-red-700 to-red-500 px-[14px] font-sans font-semibold leading-4 text-white lg:h-[44.57px]">
             <img src={EditBox} alt="Create lootbox"></img>
             <span className="whitespace-nowrap font-bold text-white xs:text-xs 2xl:text-base">Edit lootbox</span>
           </button>
         ) : (
           <div></div>
-        )}
+        )} */}
       </span>
       <div className="flex flex-row flex-wrap justify-center xs:mt-[26px] xs:gap-[1.3rem] 2xl:mt-[36px] 2xl:gap-6">
         <NFTPriceCard lootboxContent={lootboxContent} />
--- src/components/micro/NftGradientCanva.tsx
+++ src/components/micro/NftGradientCanva.tsx
@@ -19,13 +19,13 @@ const NftGradientCanva: React.FC<INftCanvaProps> = ({ children, className, cardT
         <div className="mt-1 flex flex-row items-center justify-between rounded-t-xl bg-[#1E2124] xs:mb-[1px] xs:mt-0.5  xs:h-[32px] xs:w-[155px] 2xl:mb-0.5 2xl:ml-0 2xl:h-12 2xl:w-[248px]">
           <span className="flex flex-row items-center justify-center gap-2 p-4 font-sans font-bold text-custom_white_1 xs:text-xs 2xl:text-base">
             {cardTitle}
-            {user.id && user.profile ? (
+            {/* {user.id && user.profile ? (
               <button className="flex h-5 w-5 items-center justify-center gap-2 rounded-lg bg-[#2C3034] font-sans font-semibold leading-4 text-white 2xl:h-7 2xl:w-7">
                 <img src={Trash} className="h-3 w-3 2xl:h-4 2xl:w-4" alt="Trash"></img>
               </button>
             ) : (
               <div></div>
-            )}
+            )} */}
           </span>
           <span className=" p-4 font-sans font-bold text-[#888888] xs:text-[8px] 2xl:text-base">{cardInfo}%</span>
         </div>
