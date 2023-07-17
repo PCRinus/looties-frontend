3c6ded24d140f65c38d878e082d0ddffd961fc19
florian chitu <chituflorian90@yahoo.com>
2023-07-17 12:07:27 +0300

    bug:done

--- src/components/macro/ProfileOptions.tsx
+++ src/components/macro/ProfileOptions.tsx
@@ -1,45 +1,45 @@
-import React, { useState } from "react";
-import ProfileIconRed from "../../assets/ProfileIcon.svg";
-import SettingsIcon from "../../assets/SettingsIcon.svg";
-import Games from "../../assets/Games.svg";
-import LootBoxesGrey from "../../assets/LootboxesGrey.svg";
-import Afiliates from "../../assets/AfiliatesIcon.svg";
-import TransactionIcon from "../../assets/TransactionsGrey.svg";
-import LogoutIcon from "../../assets/LogoutImg.svg";
-import InventoryIcon from "../../assets/InventoryIcon.svg";
-import SettingsRedIcon from "../../assets/SettingsRed.svg";
-import GamesRedIcon from "../../assets/GamesRed.svg";
-import InventoryRedIcon from "../../assets/InventoryRed.svg";
-import LootBoxesRed from "../../assets/LootboxesRed.svg";
-import AffiliatesRed from "../../assets/AffiliatesRed.svg";
-import TransactionsRed from "../../assets/TransactionsRed.svg";
-import LogoutRed from "../../assets/LogoutRed.svg";
-import ProfileGrey from "../../assets/ProfileGrey.svg";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../hooks/useAuth";
-import { useDispatch } from "react-redux";
-import { ReduxEvents } from "../../reducers/events";
+import React, { useState } from 'react';
+import ProfileIconRed from '../../assets/ProfileIcon.svg';
+import SettingsIcon from '../../assets/SettingsIcon.svg';
+import Games from '../../assets/Games.svg';
+import LootBoxesGrey from '../../assets/LootboxesGrey.svg';
+import Afiliates from '../../assets/AfiliatesIcon.svg';
+import TransactionIcon from '../../assets/TransactionsGrey.svg';
+import LogoutIcon from '../../assets/LogoutImg.svg';
+import InventoryIcon from '../../assets/InventoryIcon.svg';
+import SettingsRedIcon from '../../assets/SettingsRed.svg';
+import GamesRedIcon from '../../assets/GamesRed.svg';
+import InventoryRedIcon from '../../assets/InventoryRed.svg';
+import LootBoxesRed from '../../assets/LootboxesRed.svg';
+import AffiliatesRed from '../../assets/AffiliatesRed.svg';
+import TransactionsRed from '../../assets/TransactionsRed.svg';
+import LogoutRed from '../../assets/LogoutRed.svg';
+import ProfileGrey from '../../assets/ProfileGrey.svg';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { ReduxEvents } from '../../reducers/events';
+import { useLocation } from 'react-router-dom';
 
 const ProfileOptions: React.FC = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const handleLogout = () => {
-    dispatch({ type: ReduxEvents.OpenModal, payload: { modal: "LogOut" } });
+    dispatch({ type: ReduxEvents.OpenModal, payload: { modal: 'LogOut' } });
     dispatch({ type: ReduxEvents.ToggleSidebar });
   };
-  const [activeButton, setActiveButton] = useState<string | null>(null);
-
   const buttons = [
-    { name: "Profile", icon: ProfileGrey, iconRed: ProfileIconRed, path: "/profile" },
-    { name: "Settings", icon: SettingsIcon, iconRed: SettingsRedIcon, path: "settings" },
-    { name: "Games", icon: Games, iconRed: GamesRedIcon, path: "games" },
-    { name: "Inventory", icon: InventoryIcon, iconRed: InventoryRedIcon, path: "inventory" },
-    { name: "My lootboxes", icon: LootBoxesGrey, iconRed: LootBoxesRed, path: "mylootboxes" },
-    { name: "Affiliates", icon: Afiliates, iconRed: AffiliatesRed, path: "affiliates" },
-    { name: "Transactions", icon: TransactionIcon, iconRed: TransactionsRed, path: "transactions" },
-    { name: "Log out", icon: LogoutIcon, iconRed: LogoutRed, path: "" },
+    { name: 'Profile', icon: ProfileGrey, iconRed: ProfileIconRed, path: '/profile' },
+    { name: 'Settings', icon: SettingsIcon, iconRed: SettingsRedIcon, path: 'settings' },
+    { name: 'Games', icon: Games, iconRed: GamesRedIcon, path: 'games' },
+    { name: 'Inventory', icon: InventoryIcon, iconRed: InventoryRedIcon, path: 'inventory' },
+    { name: 'My lootboxes', icon: LootBoxesGrey, iconRed: LootBoxesRed, path: 'mylootboxes' },
+    { name: 'Affiliates', icon: Afiliates, iconRed: AffiliatesRed, path: 'affiliates' },
+    { name: 'Transactions', icon: TransactionIcon, iconRed: TransactionsRed, path: 'transactions' },
+    { name: 'Log out', icon: LogoutIcon, iconRed: LogoutRed, path: '' },
   ];
+
+  const [activeButton, setActiveButton] = useState<string | null>('Profile');
 
   return (
     <div className="flex flex-col items-center justify-center gap-y-4">
@@ -48,20 +48,20 @@ const ProfileOptions: React.FC = () => {
           key={name}
           onClick={() => {
             setActiveButton(name);
-            if (name === "Log out") {
+            if (name === 'Log out') {
               handleLogout();
             } else if (path) {
               navigate(path);
             }
           }}
           className={`flex items-center justify-normal rounded-xl border ${
-            activeButton === name ? "border-red-500 bg-red-500 bg-opacity-20" : "border-custom_gray_1 bg-custom_gray_1"
+            activeButton === name ? 'border-red-500 bg-red-500 bg-opacity-20' : 'border-custom_gray_1 bg-custom_gray_1'
           } xs:h-10 xs:w-[139px] 2xl:h-12 2xl:w-full 2xl:pl-[27.75px]`}
         >
           <img src={`${activeButton === name ? iconRed : icon}`} alt="svg" />
           <span
             className={`ml-2 font-sans text-base font-semibold leading-6 ${
-              activeButton === name ? "text-custom_red_1" : "text-custom_gray_2"
+              activeButton === name ? 'text-custom_red_1' : 'text-custom_gray_2'
             }`}
           >
             {name}
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,96 +1,96 @@
-import React, { useState, useEffect } from "react";
-import { Chat } from "../components/macro/Chat";
-import ProfileSideBar from "../components/macro/ProfileSideBar";
-import "../styles/micro/Profile.scss";
-import ProfilePage from "../components/macro/ProfilePage";
-import Scrollbars from "react-custom-scrollbars-2";
-import UserProfileCard from "../components/macro/UserProfileCard";
-import ProfileOptions from "../components/macro/ProfileOptions";
-import { Route, Routes } from "react-router-dom";
-import SettingsPage from "../components/macro/SettingsPage";
-import { AffiliatesPage } from "../components/macro/AffiliatesPage";
-import GameHistoryPage from "../components/macro/GameHistoryPage";
-import TransactionsPage from "../components/macro/TransactionsPage";
-import ProfileOptionsHeader from "../components/micro/ProfileOptionsHeader";
-import MyLootboxesPage from "../components/macro/MyLootboxesPage";
-import InventoryPage from "../components/macro/InventoryPage";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from 'react';
+import { Chat } from '../components/macro/Chat';
+import ProfileSideBar from '../components/macro/ProfileSideBar';
+import '../styles/micro/Profile.scss';
+import ProfilePage from '../components/macro/ProfilePage';
+import Scrollbars from 'react-custom-scrollbars-2';
+import UserProfileCard from '../components/macro/UserProfileCard';
+import ProfileOptions from '../components/macro/ProfileOptions';
+import { Route, Routes } from 'react-router-dom';
+import SettingsPage from '../components/macro/SettingsPage';
+import { AffiliatesPage } from '../components/macro/AffiliatesPage';
+import GameHistoryPage from '../components/macro/GameHistoryPage';
+import TransactionsPage from '../components/macro/TransactionsPage';
+import ProfileOptionsHeader from '../components/micro/ProfileOptionsHeader';
+import MyLootboxesPage from '../components/macro/MyLootboxesPage';
+import InventoryPage from '../components/macro/InventoryPage';
+import { useSelector } from 'react-redux';
 
 const Profile: React.FC = () => {
-  const [isXsScreen, setIsXsScreen] = useState(window.matchMedia("(max-width: 576px)").matches);
+  const [isXsScreen, setIsXsScreen] = useState(window.matchMedia('(max-width: 576px)').matches);
 
   useEffect(() => {
     const screenSizeChange = () => {
-      setIsXsScreen(window.matchMedia("(max-width: 1535px)").matches);
+      setIsXsScreen(window.matchMedia('(max-width: 1535px)').matches);
     };
 
-    window.addEventListener("resize", screenSizeChange);
+    window.addEventListener('resize', screenSizeChange);
 
     return () => {
-      window.removeEventListener("resize", screenSizeChange);
+      window.removeEventListener('resize', screenSizeChange);
     };
   }, []);
 
   return (
-      <div className="flex w-screen flex-row items-center justify-center xs:h-[calc(100vh-80px-64px)] 2xl:h-[calc(100vh-120px)]">
-        <div className="flex h-full w-full flex-col">
-          <div className="flex w-full flex-col overflow-y-scroll lg:h-full lg:flex-row lg:overflow-y-hidden">
-            {isXsScreen ? (
-                    <></>
-                ) :
+    <div className="flex w-screen flex-row items-center justify-center xs:h-[calc(100vh-80px-64px)] 2xl:h-[calc(100vh-120px)]">
+      <div className="flex h-full w-full flex-col">
+        <div className="flex w-full flex-col overflow-y-scroll lg:h-full lg:flex-row lg:overflow-y-hidden">
+          {isXsScreen ? (
+            <></>
+          ) : (
             <div className="flex h-full w-full flex-col gap-10 bg-[#151719] xs:px-6 xs:pt-8 lg:max-w-[392px] lg:overflow-y-scroll 2xl:py-[52px] 2xl:pl-[52px] 2xl:pr-0 ">
-             <div className="flex h-[806px] flex-col gap-y-8 overflow-auto rounded-xl border-[32px] border-solid border-custom_black_2 bg-custom_black_2">
+              <div className="flex h-[806px] flex-col gap-y-8 overflow-auto rounded-xl border-[32px] border-solid border-custom_black_2 bg-custom_black_2">
                 <UserProfileCard />
                 <ProfileOptions />
               </div>
             </div>
-            }
-            <div className="mb-2 w-full bg-[#151719] lg:h-full lg:max-h-full lg:overflow-y-scroll">
-              <div className="autoheight w-full">
-                  {isXsScreen ? (
-                      <div className="flex w-screen xs:h-[calc(100vh-80px-64px)] xs:flex-col 2xl:h-[calc(100vh-120px)] 2xl:flex-row">
-                      <Scrollbars autoHide universal>
-                                   <ProfileSideBar />
-                        <div className="mx-[21px]">
-                                   <Routes>
-                                     <Route path="/" element={<ProfilePage />} />
-                                     <Route path="/settings" element={<SettingsPage />} />
-                                     <Route path="/games" element={<GameHistoryPage />} />
-                                     <Route path="/inventory" element={<InventoryPage />} />
-                                     <Route path="/mylootboxes" element={<MyLootboxesPage />} />
-                                     <Route path="/affiliates" element={<AffiliatesPage />} />
-                                     <Route path="/transactions" element={<TransactionsPage />} />
-                                   </Routes>
-                        </div>
-                        <Chat />
-                          </Scrollbars>
+          )}
+          <div className="mb-2 w-full bg-[#151719] lg:h-full lg:max-h-full lg:overflow-y-scroll">
+            <div className="autoheight w-full">
+              {isXsScreen ? (
+                <div className="flex w-screen xs:h-[calc(100vh-80px-64px)] xs:flex-col 2xl:h-[calc(100vh-120px)] 2xl:flex-row">
+                  <Scrollbars autoHide universal>
+                    <ProfileSideBar />
+                    <div className="mx-[21px]">
+                      <Routes>
+                        <Route path="/" element={<ProfilePage />} />
+                        <Route path="/settings" element={<SettingsPage />} />
+                        <Route path="/games" element={<GameHistoryPage />} />
+                        <Route path="/inventory" element={<InventoryPage />} />
+                        <Route path="/mylootboxes" element={<MyLootboxesPage />} />
+                        <Route path="/affiliates" element={<AffiliatesPage />} />
+                        <Route path="/transactions" element={<TransactionsPage />} />
+                      </Routes>
+                    </div>
+                    <Chat />
+                  </Scrollbars>
+                </div>
+              ) : (
+                <>
+                  <div className="flex h-screen  w-full flex-col">
+                    <div className="grid grid-rows-1 ">
+                      <div id="options-pages-grid" className="h-full max-h-full w-full   bg-[#151719] p-[52px] ">
+                        <Routes>
+                          <Route path="/" element={<ProfilePage />} />
+                          <Route path="/settings" element={<SettingsPage />} />
+                          <Route path="/games" element={<GameHistoryPage />} />
+                          <Route path="/inventory" element={<InventoryPage />} />
+                          <Route path="/mylootboxes" element={<MyLootboxesPage />} />
+                          <Route path="/affiliates" element={<AffiliatesPage />} />
+                          <Route path="/transactions" element={<TransactionsPage />} />
+                        </Routes>
                       </div>
-                        ) : (
-                          <>
-                            <div className="flex h-screen  w-full flex-col">
-                              <div className="grid grid-rows-1 ">
-                                <div id="options-pages-grid" className="h-full max-h-full w-full   bg-[#151719] p-[52px] ">
-                                  <Routes>
-                                    <Route path="/" element={<ProfilePage />} />
-                                    <Route path="/settings" element={<SettingsPage />} />
-                                    <Route path="/games" element={<GameHistoryPage />} />
-                                    <Route path="/inventory" element={<InventoryPage />} />
-                                    <Route path="/mylootboxes" element={<MyLootboxesPage />} />
-                                    <Route path="/affiliates" element={<AffiliatesPage />} />
-                                    <Route path="/transactions" element={<TransactionsPage />} />
-                                  </Routes>
-                                </div>
-                              </div>
-                              <div className=" flex-auto bg-[#151719]"></div>
-                            </div>
-                          </>
-                        )}
-              </div>
+                    </div>
+                    <div className=" flex-auto bg-[#151719]"></div>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
-        <Chat />
       </div>
+      <Chat />
+    </div>
   );
 };
 
