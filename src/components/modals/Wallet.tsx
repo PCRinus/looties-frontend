5bb2a2c474a82f12ad66e5c3ba9337541a13286e
florian chitu <chituflorian90@yahoo.com>
2023-07-18 18:07:05 +0300

    improvement: done

--- src/components/modals/Wallet.tsx
+++ src/components/modals/Wallet.tsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import { ReduxEvents } from "../../reducers/events";
-import { ReactComponent as Close } from "../../assets/Close.svg";
-import LootiesLogo from "../../assets/Looties_wordmark 1.svg";
-import MetaMask from "../../assets/MetaMask.svg";
-import Solflare from "../../assets/Solflare.svg";
-import RedArrowDown from "../../assets/RedArrowDown.svg";
-import Phantom from "../../assets/Phantom.svg";
-import Ledger from "../../assets/Ledger.svg";
-import { useDispatch } from "react-redux";
-import { PhantomWalletName, SolflareWalletName } from "@solana/wallet-adapter-wallets";
-import { useWallet } from "@solana/wallet-adapter-react";
+import { useState } from 'react';
+import { ReduxEvents } from '../../reducers/events';
+import { ReactComponent as Close } from '../../assets/Close.svg';
+import LootiesLogo from '../../assets/Looties_wordmark 1.svg';
+import MetaMask from '../../assets/MetaMask.svg';
+import Solflare from '../../assets/Solflare.svg';
+import RedArrowDown from '../../assets/RedArrowDown.svg';
+import Phantom from '../../assets/Phantom.svg';
+import Ledger from '../../assets/Ledger.svg';
+import { useDispatch } from 'react-redux';
+import { PhantomWalletName, SolflareWalletName } from '@solana/wallet-adapter-wallets';
+import { useWallet } from '@solana/wallet-adapter-react';
 
 const Wallet = () => {
   const dispatch = useDispatch();
@@ -27,7 +27,7 @@ const Wallet = () => {
       >
         <Close />
       </div>
-      <div className='flex--column body'>
+      <div className="flex--column body">
         <div className="type mt-6 flex flex-col items-center justify-center">
           <img className="logo-img left-[73px] top-[34px] h-[36px] w-[160.94px]" src={LootiesLogo} alt="logo" />
           <span className="font-sans text-xs font-semibold">Log Into Your Account</span>
@@ -44,20 +44,20 @@ const Wallet = () => {
           <button
             className="flex h-[44.57px] w-[270px] items-center justify-center rounded-lg bg-gradient-to-t from-red-700 to-red-500 font-sans font-semibold text-white sm:w-[345px]"
             onClick={() => {
-              select(SolflareWalletName);
-            }}
-          >
-            <img className="mr-2 " src={Solflare} alt="solflare-svg-icon" />
-            Continue with Solflare
-          </button>
-          <button
-            className="flex h-[44.57px] w-[270px] items-center justify-center rounded-lg bg-gradient-to-t from-red-700 to-red-500 font-sans font-semibold text-white sm:w-[345px]"
-            onClick={() => {
               select(PhantomWalletName);
             }}
           >
             <img className="mr-2 " src={Phantom} alt="phantom-svg-icon" />
             Continue with Phantom
+          </button>
+          <button
+            className="flex h-[44.57px] w-[270px] items-center justify-center rounded-lg bg-gradient-to-t from-red-700 to-red-500 font-sans font-semibold text-white sm:w-[345px]"
+            onClick={() => {
+              select(SolflareWalletName);
+            }}
+          >
+            <img className="mr-2 " src={Solflare} alt="solflare-svg-icon" />
+            Continue with Solflare
           </button>
           {seeMore ? (
             <>
