e4cbe02d486cd3d991bcc55d534d39e2ca56c8a1
Florian Chitu <florian.chitu@codemelt.com>
2023-07-19 12:57:46 +0000

    improvement: add a volume mute button in the navbar

--- src/components/micro/VolumeButton.tsx
+++ src/components/micro/VolumeButton.tsx
@@ -1,9 +1,20 @@
-import React from "react";
-import VolumeIcon from "../../assets/VolumeIcon.svg";
+import React, { useState } from 'react';
+import VolumeIcon from '../../assets/VolumeIcon.svg';
+import VolumeIconRed from '../../assets/MuteButtonIcon.svg';
+
 const VolumeButton = () => {
+  const [isMuted, setIsMuted] = useState(false);
+
+  const handleClick = () => {
+    setIsMuted(!isMuted);
+  };
+
   return (
-    <button className="mr-4 flex items-center justify-center rounded-xl bg-custom_gray_1 xs:h-10 xs:w-10 2xl:h-12 2xl:w-12">
-      <img src={VolumeIcon} alt="volume-icon-svg" />
+    <button
+      className="mr-4 flex items-center justify-center rounded-xl bg-custom_gray_1 xs:h-10 xs:w-10 2xl:h-12 2xl:w-12"
+      onClick={handleClick}
+    >
+      <img src={isMuted ? VolumeIconRed : VolumeIcon} alt="volume-icon-svg" />
     </button>
   );
 };
